5058f08a686433646ca35ccbcea782a4e3224ee0
kartikpande123 <kartikpande67@gmail.com>
2025-04-05 20:19:04 +0530

    Fixed API URL to point to the correct domain

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ const port = 2025;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: '*'
+}));
 
 // Firestore setup
 const firestore = admin.firestore();
